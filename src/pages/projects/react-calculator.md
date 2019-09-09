e81ad7fce8ed9fb521db00a71d0e4e6b3057379a
Kevin Reynolds <kevintreynolds30@gmail.com>
2019-09-09 01:19:35 -0700

    fix project errors

--- src/pages/projects/react-calculator.md
+++ src/pages/projects/react-calculator.md
@@ -1,4 +1,5 @@
 ---
+templateKey: project
 title: React Calculator
 projectDate: 2018-03-01T07:07:22.301Z
 projectImage: /img/jumbotron.jpg
---
+++ src/templates/project.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { kebabCase } from 'lodash'
+import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Content, { HTMLContent } from '../components/Content'
+
+export const ProjectTemplate = ({
+  content,
+  contentComponent,
+  tags,
+  title,
+  helmet,
+}) => {
+  const PostContent = contentComponent || Content
+
+  return (
+    <section className="section">
+      {helmet || ''}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <PostContent content={content} />
+            {tags && tags.length ? (
+              <div style={{ marginTop: `4rem` }}>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map(tag => (
+                    <li key={tag + `tag`}>
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+ProjectTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+}
+
+const Project = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <ProjectTemplate
+      content={post.html}
+      contentComponent={HTMLContent}
+      description={post.frontmatter.description}
+      helmet={
+        <Helmet titleTemplate="%s | Blog">
+          <title>{`${post.frontmatter.title}`}</title>
+          <meta
+            name="description"
+            content={`${'description'}`}
+          />
+        </Helmet>
+      }
+      tags={post.frontmatter.tags}
+      title={post.frontmatter.title}
+    />
+  )
+}
+
+Project.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
+
+export default Project
+
+export const pageQuery = graphql`
+  query ProjectByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        projectDate(formatString: "MMMM DD, YYYY")
+        title
+        tags
+      }
+    }
+  }
+`
